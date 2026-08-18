994e1c309820e07eb5425c07eeb3f247b957095f
ads <tcakeerthanamai@gmail.com>
2026-08-18 15:20:16 +0530

    Initial commit

---
+++ basic.js
@@ -0,0 +1,15 @@
+let a = 150;
+if(a>=18 && a<=150)
+    {
+    console.log("eligible")
+
+    }
+else if(a>150)
+    {
+        console.log("not possible to live")
+    }
+else
+    {
+        console.log("not eligible")
+
+    }
---
+++ guviassign1.js
@@ -0,0 +1,21 @@
+let income = 20000;
+let food = 3000;
+let travel = 2000;
+let shopping = 1500;
+let other = 1000;
+let totalExpenses = food + travel + shopping + other;
+let savings = income - totalExpenses;
+
+console.log("Income: ₹" + income);
+
+console.log("Food Expense: ₹" + food);
+
+console.log("Travel Expense: ₹" + travel);
+
+console.log("Shopping Expense: ₹" + shopping);
+
+console.log("Other Expense: ₹" + other);
+
+console.log("Total Expenses: ₹" + totalExpenses);
+
+console.log("Savings: ₹" + savings);
